ec8152343a87881693d875959649b71488d6d767
Shekhar Kumar <shekhark221@gmail.com>
2017-12-17 15:12:11 -0800

    background

---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	
+	<!-- // in CSS  -->
+	<style type="text/css">
+.bg1 
+{
+	background-color:black; 
+  background-image: url(11.jpg);
+   /*background-repeat: no-repeat;*/
+   background-size:1300px 800px; 
+}
+
+
+</style>
+</head>
+<body class="bg1">
+	
+</body>
+</html>
