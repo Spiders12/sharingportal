0dd4d36c9d2811622d23afdeabd68705b3c82419
vaibhav28398 <31373963+vaibhav28398@users.noreply.github.com>
2017-12-19 23:51:38 +0530

    added profile

---
+++ profile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Share</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand active" href="index.php">SHARE</a>
+    </div>
+    <div class="colllapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav">
+      <li class="active" ><a href="profile.php">Profile</a></li>
+      <li ><a href="aboutus.php" >About us</a></li>
+      <li ><a href="#footer">Contact us</a></li>
+    </ul>
+  </div>
+</div>
+</nav>
+<div class="content">
+</div>
+
+</body>
+</html>
