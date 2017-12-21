c278c9478e016fbf38f2c196d1b99442d6f93c3e
Shekhar Kumar <shekhark221@gmail.com>
2017-12-20 23:43:45 -0800

    login and signup form 20 dec

---
+++ form/login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$db=new MYSQLi("localhost","root","","interview");
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+	$email=$_POST["email"];
+	$password=$_POST["password"];
+	
+	$sql=" SElECT * FROM signup WHERE email='$email' AND password='$password'";
+	$result=mysqli_query($db,$sql);
+	if(mysqli_num_rows($result)==1)
+	{
+	
+		$_SESSION['message']="You are now logged in";
+		$_SESSION['name']=$name;
+			
+		header("location:home.php");
+		}
+		else{
+			echo "<script>alert('Incorrect username/password');window.location='/form/';</script>";
+		}
+}
+?>
