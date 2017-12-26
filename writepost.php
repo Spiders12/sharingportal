e40d42fa4208cb9882c157b7a5f8cd993a79c7a9
vaibhav28398 <31373963+vaibhav28398@users.noreply.github.com>
2017-12-26 16:56:19 +0530

    26-12-2017

--- writepost.php
+++ writepost.php
@@ -10,36 +10,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-  <script type="text/javascript">
-  $(function(){ /* to make sure the script runs after page load */
-
-  $('.post').each(function(event){ /* select all divs with the item class */
-  
-    var max_length = 150; /* set the max content length before a read more link will be added */
-    
-    if($(this).html().length > max_length){ /* check for content length */
-      
-      var short_content   = $(this).html().substr(0,max_length); /* split the content in two parts */
-      var long_content  = $(this).html().substr(max_length);
-      
-      $(this).html(short_content+
-             '<a href="#" class="read_more"><br/>Read More</a>'+
-             '<span class="more_text" style="display:none;">'+long_content+'</span>'); /* Alter the html to allow the read more functionality */
-             
-      $(this).find('a.read_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
- 
-        event.preventDefault(); /* prevent the a from changing the url */
-        $(this).hide(); /* hide the read more button */
-        $(this).parents('.post').find('.more_text').show(); /* show the .more_text span */
-     
-      });
-      
-    }
-    
-  });
- 
- 
-});</script>
 </head>
 <body>
 	<nav class="navbar navbar-inverse navbar-fixed-top ">
@@ -78,14 +48,14 @@
   
   <div class="menu card" id="fixme" >
     <table style="width:100%;" >
-      <tr class="active1 active12">
+      <tr class="active1">
         <td >
           <a href="profile.php"><h3>Profile<i class="iconmenu fa fa-drivers-license"></i> </h3></a>
         </td>
       </tr>
-      <tr class="active1">
+      <tr class="active1 active12">
         <td>
-          <a href="writepost.php"><h3>Write Post<i class="iconmenu fa fa-plus-square"></i></h3></a>
+          <a href="#"><h3>Write Post<i class="iconmenu fa fa-plus-square"></i></h3></a>
         </td>
       </tr>
       <tr class="active1">
@@ -108,22 +78,6 @@
   </div>
 </div>
 <div class="col-md-7 mainbar">
-  <div class="textbox card"><textarea class="profilepost" placeholder="Write something about you........."></textarea></div>
-	  <?php 
-    $i=10;
-    while($i!=0){
-      echo "<div class='post card'><p>This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.
-      This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.
-      This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.</p>
-      <p>This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.
-      This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.
-      This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.
-      This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.This is sample text.</p></div>";
-      $i--;
-    } ?>
-</div>
-<div class="col-md-2"></div>
-<div class="col-md-7 mainbarwritepost">
   <div class="textbox card"><textarea class="profilepost" placeholder="Write something about you........."></textarea></div>
   </div>
 <div class="col-md-2"></div>
